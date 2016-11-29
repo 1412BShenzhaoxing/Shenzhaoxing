572b75ee8be3aac45fc45dfe9fa1548646abbaaa
zhangdongxuh <1787092963@qq.com>
2016-11-29 16:36:32 +0800

    iuyt908

--- app/src/main/java/com/bwie/recyclerview/qwqwqw.java
+++ app/src/main/java/com/bwie/recyclerview/qwqwqw.java
@@ -2,7 +2,7 @@ package com.bwie.recyclerview;
 
 /**
  * 1.类的用途
- * 2.@ renshaoshuai
+ * 2.@ renshaoshuaizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
  * 3.@ 2016/11/29.
  */
 public class qwqwqw {
