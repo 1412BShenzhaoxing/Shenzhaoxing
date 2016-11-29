0cf06385d30e1c129af4634e9f78f4850518b4e1
Administrator <1207066631@qq.com>
2016-11-29 16:18:05 +0800

    xiugai class

--- app/src/main/java/com/bwie/recyclerview/qwqqwqwqw.java
+++ app/src/main/java/com/bwie/recyclerview/qwqqwqwqw.java
@@ -6,4 +6,11 @@ package com.bwie.recyclerview;
  * 3.@ 2016/11/29.
  */
 public class qwqqwqwqw {
+    public String name;
+    public String name1;
+    public String name2;public String name6;
+    public String name3;
+    public String name4;
+    public String name5;
+
 }
